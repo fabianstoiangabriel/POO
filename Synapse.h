b073eacc13c27db2a9bb938fb19a54ab16144d0e
fabianstoiangabriel <fabianstoian6@gmail.com>
2026-05-02 16:26:27 +0300

    Refactor Synapse class constructor and methods

--- Synapse.h
+++ Synapse.h
@@ -8,18 +8,43 @@ private:
     Neuron* pre;
     Neuron* post;
     double strength;
-    double synapticDelay;
-    double releaseTime;
+    double synapticDelay = 0.0;
+    double releaseTime = -1.0;
 
 public:
-    Synapse(Neuron* pre, Neuron* post, double strength);
-    Synapse(const Synapse& other, Neuron* newPre, Neuron* newPost);
+    Synapse(Neuron* pre, Neuron* post, double strength)
+        : pre(pre), post(post), strength(strength) {}
 
-    void transmit(double time);
-    Neuron* getPre() const;
-    Neuron* getPost() const;
-    double getStrength() const;
-    void setSynapticDelay(double delay);
+    Synapse(const Synapse& other, Neuron* newPre, Neuron* newPost)
+        : pre(newPre), post(newPost), strength(other.strength), 
+          synapticDelay(other.synapticDelay) {}
+
+    void transmit(double time = 0.0) {
+        double V_pre = pre->getVoltage();
+        if (V_pre > 0 && releaseTime < 0) {
+            releaseTime = time + synapticDelay;
+        }
+        if (time >= releaseTime && releaseTime >= 0) {
+            post->setExternalCurrent(strength);
+            releaseTime = -1.0;
+        }
+    }
+
+    Neuron* getPre() const { 
+        return pre; 
+    }
+
+    Neuron* getPost() const { 
+        return post; 
+    }
+
+    double getStrength() const { 
+        return strength; 
+    }
+
+    void setSynapticDelay(double delay) { 
+        synapticDelay = delay; 
+    }
 };
 
 #endif // SYNAPSE_H
