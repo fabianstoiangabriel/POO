0a0c6c6feaa0c9858d6d3c8c5af4d596199e6bc2
fabianstoiangabriel <fabianstoian6@gmail.com>
2026-05-02 16:23:25 +0300

    Refactor Gate class and add multiple gate implementations

--- Gate.h
+++ Gate.h
@@ -1,8 +1,8 @@
 #ifndef GATE_H
 #define GATE_H
 
-#include <iostream>
 #include <string>
+#include <cmath>
 
 class Gate {
 private:
@@ -12,17 +12,116 @@ protected:
     double P;
 
 public:
-    Gate(double initialP);
+    Gate(double initialP) : P(initialP) {}
     virtual ~Gate() = default;
-    Gate(const Gate& other);
-
+    Gate(const Gate& other) : P(other.P) {}
+    
     virtual double alpha(double V) = 0;
     virtual double beta(double V) = 0;
-    void updD(double V, double dt);
-    double steadyState(double V);
-    double timeConstant(double V);
-    double getP() const;
-    void print(std::ostream& os) const;
+    
+    void updD(double V, double dt) {
+        double dP = alpha(V) * (1 - P) - beta(V) * P;
+        P += dP * dt;
+    }
+    
+    double steadyState(double V) { 
+        return alpha(V) / (alpha(V) + beta(V)); 
+    }
+
+    double timeConstant(double V) { 
+        return 1.0 / (alpha(V) + beta(V)); 
+    }
+    
+    double getP() const { return P; }
+
+    void print(std::ostream& os) const {
+        os << "Gate [P=" << P << ", type=" << gateType() << "]";
+    }
+};
+
+class MGate : public Gate {
+private:
+    std::string gateType() const override { 
+        return "M (Na activation)"; 
+    }
+public:
+    MGate(double initialP) : Gate(initialP) {}
+    
+    double alpha(double V) override {
+        return 0.1 * (V + 40) / (1 - exp(-(V + 40) / 10));
+    }
+    
+    double beta(double V) override {
+        return 4 * exp(-(V + 65) / 18);
+    }
+};
+
+class HGate : public Gate {
+private:
+    std::string gateType() const override { 
+        return "H (K activation)"; 
+    }
+public:
+    HGate(double initialP) : Gate(initialP) {}
+    
+    double alpha(double V) override {
+        return 0.07 * exp(-(V + 65) / 20);
+    }
+    
+    double beta(double V) override {
+        return 1 / (exp(-(V + 35) / 10) + 1);
+    }
+};
+
+class NGate : public Gate {
+private:
+    std::string gateType() const override { 
+        return "N (K inactivation)"; 
+    }
+public:
+    NGate(double initialP) : Gate(initialP) {}
+    
+    double alpha(double V) override {
+        return 0.01 * (V + 55) / (1 - exp(-(V + 55) / 10));
+    }
+    
+    double beta(double V) override {
+        return 0.125 * exp(-(V + 65) / 80);
+    }
+};
+
+class CaMGate : public Gate {
+private:
+    std::string gateType() const override { 
+        return "CaM (Ca activation)"; 
+    }
+public:
+    CaMGate(double initialP) : Gate(initialP) {}
+    
+    double alpha(double V) override {
+        return 0.055 * (V + 27) / (1 - exp(-(V + 27) / 3.8));
+    }
+    
+    double beta(double V) override {
+        return 0.94 * exp(-(V + 75) / 17);
+    }
+};
+
+class CaHGate : public Gate {
+private:
+    std::string gateType() const override { 
+        return "CaH (Ca inactivation)"; 
+    }
+public:
+    CaHGate(double initialP) : Gate(initialP) {}
+    
+    double alpha(double V) override {
+        return 0.000457 * exp(-(V + 13) / 50);
+    }
+    
+    double beta(double V) override {
+        return 0.0065 / (1 + exp(-(V + 15) / 28));
+    }
 };
 
 #endif // GATE_H
