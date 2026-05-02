5d74cd8c7e9a6a50054f6f5cb8c6df6c2546549c
fabianstoiangabriel <fabianstoian6@gmail.com>
2026-05-02 16:24:59 +0300

    Refactor exception handling in SimulationExceptions.h

--- SimulationExceptions.h
+++ SimulationExceptions.h
@@ -1,26 +1,34 @@
-#ifndef SIMULATION_EXCEPTIONS_H
-#define SIMULATION_EXCEPTIONS_H
+#ifndef SYSTEMERRORS_H
+#define SYSTEMERRORS_H
 
 #include <stdexcept>
 
 class SimulationException : public std::exception {
 public:
-    const char* what() const noexcept override;
+    const char* what() const noexcept override {
+        return "Simulation error!";
+    }
 };
 
 class InvalidConductanceException : public SimulationException {
 public:
-    const char* what() const noexcept override;
+    const char* what() const noexcept override {
+        return "Conductance cannot be negative!";
+    }
 };
 
 class InvalidVoltageException : public SimulationException {
-public:
-    const char* what() const noexcept override;
+public:    
+    const char* what() const noexcept override {
+        return "Voltage out of range!";
+    }
 };
 
 class TimestepException : public SimulationException {
 public:
-    const char* what() const noexcept override;
+    const char* what() const noexcept override {
+        return "Timestep too large, simulation unstable!";
+    }
 };
 
-#endif // SIMULATION_EXCEPTIONS_H
+#endif // SYSTEMERRORS_H
