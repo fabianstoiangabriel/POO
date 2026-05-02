c3b7a1f65e59ed5e2b5a959fe454707acd61e5e0
fabianstoiangabriel <fabianstoian6@gmail.com>
2026-05-02 16:19:34 +0300

    Implement constructor for Amygdala class
    Refactor Amygdala constructor to initialize neurons and synapses.

--- Amygdala.h
+++ Amygdala.h
@@ -9,8 +9,40 @@ private:
     int inhibitory;
 
 public:
-    Amygdala(int numNeurons);
-    void setFearState(bool fear);
+    Amygdala(int numNeurons) : NeuralNetwork() {
+        excitatory = numNeurons * 0.8;
+        inhibitory = numNeurons * 0.2;
+
+        std::vector<Neuron*> excNeurons;
+        std::vector<Neuron*> inhNeurons;
+
+        for (int i = 0; i < excitatory; i++) {
+            Neuron* n = new Neuron(-65.0);
+            n->setExternalCurrent(10.0);
+            addNeuron(n);
+            excNeurons.push_back(n);
+        }
+
+        for (int i = 0; i < inhibitory; i++) {
+            Neuron* n = new Neuron(-65.0);
+            addNeuron(n);
+            inhNeurons.push_back(n);
+        }
+
+        for (auto pre : excNeurons)
+            for (auto post : excNeurons)
+                if (pre != post)
+                    addSynapse(new Synapse(pre, post, 0.5));
+
+        for (auto pre : inhNeurons)
+            for (auto post : excNeurons)
+                addSynapse(new Synapse(pre, post, -0.5));
+    }
+
+    void setFearState(bool fear) {
+        for (auto neuron : neurons)
+            neuron->setExternalCurrent(fear ? 15.0 : 2.0);
+    }
 };
 
 #endif // AMYGDALA_H
